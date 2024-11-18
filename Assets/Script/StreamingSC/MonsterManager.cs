0e66cfb7d7c4caf7ad0e5ac6d95ee8fb4afa76fa
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-11-18 14:58:30 +0900

    Update SC Commit
    Monster Spawn Logic Update

--- Assets/Script/StreamingSC/MonsterManager.cs
+++ Assets/Script/StreamingSC/MonsterManager.cs
@@ -21,35 +21,74 @@ public class MonsterManager : MonoBehaviour
 
     private GameObject player;
 
+    private bool isSpawning; // 중복 호출 방지 플래그
+    private bool isDespawning;
+
     private void Start()
     {
         player = GameObject.Find("Player");
+        isSpawning = false;
+        isDespawning = false;
     }
 
     private void Update()
     {
         //특정 범위에 들어간 경우 몬스터 Spawn.
-        CallMonsterSpawn();
+        if (!isSpawning)
+        {
+            StartCoroutine(CallSpawnMonster());
+        }
+
+        if (!isDespawning)
+        {
+            StartCoroutine(CallDespawnMonster());
+        }
     }
 
     //활성화 되어있는 Sector의 몬스터 스폰 오브젝트를 가져온다.
     Dictionary<Vector2Int, SectorMonsterSpawner> monsterSpawners = new Dictionary<Vector2Int, SectorMonsterSpawner>();
     int monsterSpawnCallRange = 1;
 
-    void CallMonsterSpawn()
+    IEnumerator CallSpawnMonster()
     {
+        isSpawning = true;
         Vector2Int currentSector = MapStreamingManager.Instance.GetSector(player.transform.position);
         for (int x = -monsterSpawnCallRange; x <= monsterSpawnCallRange; x++)
         {
             for (int y = -monsterSpawnCallRange; y <= monsterSpawnCallRange; y++)
             {
                 Vector2Int spawnCallSector = currentSector + new Vector2Int(x, y);
-                
+                while (!monsterSpawners.ContainsKey(spawnCallSector))
+                {
+                    yield return null;
+                }
+                monsterSpawners[spawnCallSector].SpawnMonster();
             }
         }
     }
 
-    //외부 씬에서 Load 되었을때 이 중앙 처리스크립트로 가져오기.
+    IEnumerator CallDespawnMonster()
+    {
+        //캐릭터가 범위를 벗어 났을때..
+        Vector2Int currentSector = MapStreamingManager.Instance.GetSector(player.transform.position);
+
+        isDespawning = true;
+
+        //현재 Dictionary에 있는 스포너들의 키(Vector2Int)와 현재 Sector를 비교
+        foreach (var sector in monsterSpawners.Keys)
+        {
+            if(Mathf.Abs(sector.x - currentSector.x) > monsterSpawnCallRange && Mathf.Abs(sector.y - currentSector.y) > monsterSpawnCallRange)
+            {
+                if (monsterSpawners.ContainsKey(sector))
+                {
+                    monsterSpawners[sector].DespawnMonster(); // 디스폰 처리
+                }
+            }
+        }
+
+        yield return null;
+    }
+
     public void AddMonsterSpawnerSC(Vector2Int sector, SectorMonsterSpawner spawner)
     {
         if (!monsterSpawners.ContainsKey(sector))
@@ -59,7 +98,6 @@ public class MonsterManager : MonoBehaviour
         }
     }
 
-    //UnLoad시..
     public void RemoveMonsterSpawnerSC(Vector2Int sector)
     {
         if (monsterSpawners.ContainsKey(sector))
--- Assets/Script/StreamingSC/SectorMonsterSpawner.cs
+++ Assets/Script/StreamingSC/SectorMonsterSpawner.cs
@@ -7,29 +7,28 @@ public class SectorMonsterSpawner : MonoBehaviour
     // Scene 내부에 존재하며 Scene이 활성화 되었을때 대기하고있다.
     // 캐릭터와의 거리가 1Sector 정도 왔을 때, 몬스터를 활성화 한다.
     public static SectorMonsterSpawner localInstance;
+    
+    public static ObjectPooling ObjectPooling;
+    public List<GameObject> spawnedMonster;
+
 
     private void Awake()
     {
         localInstance = this;
+        spawnedMonster = new List<GameObject>();
     }
 
-    public GameObject monsterPrefab;
-    //public Transform[] spawnPrefab;
     public int spawnCount;          //섹터별로 별도로 세팅.
 
-    private Queue<GameObject> monsterPool;
     public Vector2Int sectorVec;
-
+    
     void Start()
     {
-        //초기화 횟수 필요.    
-        monsterPool = new Queue<GameObject>();
-        SpawnMonster();
+        ObjectPooling = GetComponent<ObjectPooling>();
     }
 
     private void OnEnable()
     {
-        //활성화 된 경우... 중앙 처리 오브젝트에 SC 참조를 넘긴다.
         MonsterManager.instance.AddMonsterSpawnerSC(sectorVec, localInstance);
     }
 
@@ -40,7 +39,18 @@ public class SectorMonsterSpawner : MonoBehaviour
 
     public void SpawnMonster()
     {
-        //test용으로 현재 스폰서의 위치에 소환한다 [스폰서의 중앙]
-        GameObject monster = Instantiate(monsterPrefab, transform.position, transform.rotation);
+        GameObject monster = ObjectPooling.GetObject(transform.position, transform.rotation);   
+        spawnedMonster.Add(monster);
+    }
+
+    public void DespawnMonster()
+    {
+        if (spawnedMonster.Count > 0)
+        {
+            foreach (GameObject monster in spawnedMonster)
+            {
+                ObjectPooling.ReturnObject(monster);
+            }
+        }
     }
 }
