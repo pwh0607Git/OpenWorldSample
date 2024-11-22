8ece75a1dc2c7079d0327821a11d7e7f1158b4ad
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-11-22 18:17:05 +0900

    SC update Commit(3)
    스폰할 몬스터 종류 inspector에 추가할 수 있도록 설정

--- Assets/Script/ObjectPooling.cs
+++ Assets/Script/ObjectPooling.cs
@@ -5,37 +5,75 @@ using UnityEngine;
 
 public class ObjectPooling : MonoBehaviour
 {
-    public GameObject monsterPrefab;
-    public int maxCount = 5;
-
-    private Queue<GameObject> monsterPools = new Queue<GameObject>();
-
-    private void Awake()
+    [System.Serializable]
+    public class Monster
     {
-        for(int i = 0; i < maxCount; i++)
+        public string name;
+        public int HP;
+        public int damage;
+        public float speed;
+        public int size;
+        public GameObject prefab;
+        public int count;
+
+        public Monster(int HP, int size, int damage, float speed, GameObject prefab, int count)
         {
-            GameObject monster = Instantiate(monsterPrefab);
-            monster.SetActive(false);
-            monsterPools.Enqueue(monster);
+            this.name = prefab.tag;
+            this.HP = HP;
+            this.damage = damage;
+            this.speed = speed;
+            this.size = size;
+            this.prefab = prefab;
+            this.count = count;
         }
     }
 
-    public GameObject GetObject(Vector3 position, Quaternion rotation)
+    //차후 사용예정.
+    public class Boss : Monster
     {
-        if(monsterPools.Count > 0)
+        public Boss(int HP, int size, int damage, float speed, GameObject prefab, int count) : base(size, HP, damage, speed, prefab, count) { }
+    }
+
+    public List<Monster> monsterPrefabs = new List<Monster>();                        //inspector에서 프리팹 추가하기.
+    private Dictionary<string, Queue<GameObject>> monsterDictionary;            //monsterTag , queue
+
+    private void Awake()
+    {
+        monsterDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Monster mon in monsterPrefabs)
         {
-            GameObject monster = monsterPools.Dequeue();
-            monster.transform.position = position;
-            monster.transform.rotation = rotation;
-            monster.SetActive(true);
-            return monster;
+            Queue<GameObject> pool = new Queue<GameObject>();
+
+            for (int i = 0; i < mon.count; i++)
+            {
+                GameObject instance = Instantiate(mon.prefab);
+                instance.tag = mon.name; // tag로 구분
+                instance.SetActive(false);
+                pool.Enqueue(instance);
+            }
+            monsterDictionary.Add(mon.name, pool);
         }
+    }
+
+    public GameObject GetObject(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (monsterDictionary.ContainsKey(tag) && monsterDictionary[tag].Count > 0)
+        {
+            GameObject instance = monsterDictionary[tag].Dequeue();
+            instance.transform.position = position;
+            instance.transform.rotation = rotation;
+            instance.SetActive(true);
+            return instance;
+        }
+        Debug.LogWarning($"No objects available in pool for tag: {tag}");
         return null;
     }
 
     public void ReturnObject(GameObject monster)
     {
         monster.SetActive(false);
-        monsterPools.Enqueue(monster);
+        string tag = monster.tag;
+        monsterDictionary[tag].Enqueue(monster);
     }
 }
