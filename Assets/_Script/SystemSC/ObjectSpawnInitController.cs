f63e1a61663556803970d5502421853b13515706
pwh0607Git <pwhof0607@gmail.com>
2025-01-15 19:32:16 +0900

    v0.0.17] edit : 몬스터 terrain 스폰 로직 수정
    - 스폰시 y값을 보정하여 땅위에 오도록 설정

--- Assets/_Script/SystemSC/ObjectSpawnInitController.cs
+++ Assets/_Script/SystemSC/ObjectSpawnInitController.cs
@@ -5,16 +5,15 @@ public class ObjectSpawnInitController : MonoBehaviour
     public Vector3 originalPosition { get; set; }
 
     public void SetOntheFloor(){
-        Ray upRay = new Ray(transform.position, Vector3.up);
-        Ray downRay = new Ray(transform.position, Vector3.down);
+        Ray upRay = new Ray(transform.position + Vector3.down * 1f, Vector3.up);
+        Ray downRay = new Ray(transform.position + Vector3.up * 1f, Vector3.down);
         RaycastHit hit;
-
+        
         //바닥이 위에 있다면... 바닥 면에 맞닿아야한다.
         if(Physics.Raycast(upRay, out hit, Mathf.Infinity, LayerMask.GetMask("Level")) || Physics.Raycast(downRay, out hit, Mathf.Infinity, LayerMask.GetMask("Level"))){
-            if(hit.transform.CompareTag("Floor")){
-                Vector3 newSpawnPos = new Vector3(transform.position.x, hit.transform.position.y, transform.position.z);
-                transform.position = newSpawnPos;
-                originalPosition = newSpawnPos;
+            
+            if(hit.transform.CompareTag("Floor") || hit.transform.GetComponent<Terrain>() != null){
+                originalPosition = new Vector3(transform.position.x, hit.point.y, transform.position.z);
             }
         }
     }
