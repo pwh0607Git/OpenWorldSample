50c7371b6c2c9d55b36d1c637253eb2362e246f6
pwh0607Git <pwhof0607@gmail.com>
2025-02-10 18:57:33 +0900

    v0.0.35 edit : 보스 monster 공격 로직 수정

--- Assets/_Script/MonsterSC/_BossLogic/ForestGolemController.cs
+++ Assets/_Script/MonsterSC/_BossLogic/ForestGolemController.cs
@@ -1,36 +1,37 @@
+using System;
 using UnityEngine;
 
-public class BasicBossState{
-    public int HP;
-    public float moveSpeed;
-    public BasicBossState(int HP, float moveSpeed){
-        this.HP = HP;
-        this.moveSpeed = moveSpeed;
-    }
-}
 
-public class ForestGolemController : MonoBehaviour
-{
+public class ForestGolemController : Boss
+{    
     public GameObject rockPrefab;
     [SerializeField] GameObject takenRock;  //현재 손에 쥐고 있는 돌.
     public Transform takenRockPosition;
     [SerializeField] GameObject target;
-    public Animator animator;
+    bool isPerformingStage = false;
+
     [SerializeField] bool isAttacking;
-    
     [SerializeField] float ThrowAttackTime = 2.0f;
-    [SerializeField] float timer;
+    [SerializeField] float timer;    
+    private CharacterController controller;
+    [SerializeField] Vector3 originalPosition;
+
     void Start()
     {
         target = GameObject.FindWithTag("Player");
         isAttacking = false;
         TryGetComponent(out animator);
-    
+        originalPosition = transform.position;
         timer = 0.0f;
+
+        animator.SetTrigger("StartStage");
     }
 
    void Update()
     {   
+        if(Input.GetKeyDown(KeyCode.Space)) Down();
+        if(isPerformingStage) return;
+
         if(InShortRangeTarget()){
             if(isAttacking) return;
             animator.SetTrigger("Short-RangeAttack");
@@ -44,7 +45,20 @@ public class ForestGolemController : MonoBehaviour
         if(timer >= ThrowAttackTime){
             animator.SetTrigger("Long-RangeAttack");
         }
-        // HandleAttack();
+    }
+    
+    void StartBossStage(){
+        animator.SetTrigger("Start");
+        isPerformingStage = true;
+    }
+
+    void EndBossStage(){
+        isPerformingStage = false;
+        ReturnOriginalPosition();
+    }
+
+    void ReturnOriginalPosition(){
+        controller.Move(originalPosition * 10f * Time.deltaTime);
     }
 
     void LookAtTarget(){
@@ -61,27 +75,31 @@ public class ForestGolemController : MonoBehaviour
         return distanceToTarget <= shortAttackRange;
     }
 
-    public void Punch(){        //펀지는 player를 넉백 시킨다.
-        Vector3 attackOffset = transform.localPosition + Vector3.up/2 + transform.forward;
-        Collider[] hitTargets = Physics.OverlapSphere(attackOffset, shortAttackRange);
+    public void Punch(){                //펀치는 player를 넉백 시킨다.
+        Vector3 attackOffset = transform.localPosition + Vector3.up * 2.3f + transform.forward * 4 + transform.right * -1f;
+        Collider[] hitTargets = Physics.OverlapSphere(attackOffset, availableDamageZone);
         foreach(var target in  hitTargets)
         {
             if (target.CompareTag("Player"))
             {
                 Debug.Log("Attack Punch To Player!!!");
                 target.GetComponentInChildren<PlayerController>().PlayerTakeDamage(10);         //TestPower
+                KnockBackTarget();
             }
         }
     }
-
+    
+    public float knockBackPower = 100f;
     void KnockBackTarget(){
         //넉백 방향 계산하기
         Vector3 direction = (target.transform.position - transform.position).normalized;
-        direction.y = 0;            //테스트로 수평으로만 수행하도록 설정.
-        target.GetComponentInChildren<PlayerController>().ApplyKnockBack(direction);
+        direction.y = 0;                                                                //테스트로 수평으로만 수행하도록 설정.
+        direction += Vector3.up;
+        target.GetComponentInChildren<PlayerController>().ApplyKnockBack(direction * knockBackPower);
     }
     #endregion
 
+    #region Attack Animaton Event
     public void StartThrowRock(){
         Debug.Log("돌 만들기");
         takenRock = Instantiate(rockPrefab, takenRockPosition);
@@ -98,6 +116,7 @@ public class ForestGolemController : MonoBehaviour
         isAttacking = false;
         timer = 0.0f;
     }
+    #endregion
 
     [SerializeField] float availableDamageZone;
     void OnDrawGizmos(){
@@ -105,7 +124,7 @@ public class ForestGolemController : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position + Vector3.up * 3, shortAttackRange);
         
         //TakeDamageArea
-        Vector3 attackOffset = transform.localPosition + Vector3.up * 2.3f + transform.forward * 4 + transform.right * -1.5f;
+        Vector3 attackOffset = transform.localPosition + Vector3.up * 2.3f + transform.forward * 4 + transform.right * -1f;
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackOffset, availableDamageZone);
     }
