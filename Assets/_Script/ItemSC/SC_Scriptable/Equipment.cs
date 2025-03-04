505987b4fa9562bc3fdc253ba648d4763d2b2a18
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2025-03-04 23:34:43 +0900

    v0.0.57 edit : Actionbar 리팩토링 수행 완료

--- Assets/_Script/ItemSC/SC_Scriptable/Equipment.cs
+++ Assets/_Script/ItemSC/SC_Scriptable/Equipment.cs
@@ -12,22 +12,18 @@ public enum EquipmentType
 public class Equipment : ItemData
 {
     public EquipmentType subType;
-    
+
+    public int maxHpBonus = 0;
+    public int attackBonus = 0;
+    public int defendBonus = 0;
+    public float speedBonus = 0;
+
     private void OnEnable()
     {
         itemType = ItemType.Equipment;
     }
 
     public override void Use(){
-        State state = PlayerController.player.myState;
-        PlayerController.player.SetEquipment(model);
-        state.EquipItem(this);
-    }
-
-    public void Detach()
-    {
-        State state = PlayerController.player.myState;
-        PlayerController.player.CleanEquipment();
-        state.DetachItem(this);
+        // 슬롯에서 값 처리하기.
     }
 }
